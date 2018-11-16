2689d86c18d79b8a0e908d75a5a8640a90cc4904
James Griffin <james@unsupervised.ca>
2018-11-15 21:38:55 -0400

    Initial Commit

---
+++ Jot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Jot
+//
+//  Created by James Griffin-Allwood on 2018-11-15.
+//  Copyright © 2018 James Griffin-Allwood. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
