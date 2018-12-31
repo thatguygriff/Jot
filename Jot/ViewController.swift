98200bce39e0b85f8c7e1e08ffc13a2e0bd63547
James Griffin <james@unsupervised.ca>
2018-12-31 15:36:40 -0400

    Initial window layout

---
+++ Jot/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Jot
+//
+//  Created by James Griffin-Allwood on 2018-12-31.
+//  Copyright © 2018 James Griffin-Allwood. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
